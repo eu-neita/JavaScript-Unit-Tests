d4297463adf9dd9f061653272ea278fab7df5aed
neita <neitass.music@gmail.com>
2023-01-23 19:42:10 -0300

    termina 9 exercicio

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -44,7 +44,13 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
+    expect(getCharacter()).toBeUndefined();
+    expect(getCharacter('Arya')).toEqual({name: 'Arya Stark', class: 'Rogue', phrases: [ 'Not today', 'A girl has no name.' ]});
+    expect(getCharacter('brienne')).toEqual({name: 'Brienne Tarth',class: 'Knight',phrases: ['Im No Lady, Your Grace.','I, Brienne Of Tarth, Sentence You To Die.']});
+    expect(getCharacter('melissandre')).toEqual({name: 'Melissandre',class: 'Necromancer',phrases: ['Death By Fire Is The Purest Death.','For The Night Is Dark And Full Of Terrors.']});
+    expect(getCharacter('melissandre') || getCharacter('Melissandre')).toEqual({name: 'Melissandre',class: 'Necromancer',phrases: ['Death By Fire Is The Purest Death.','For The Night Is Dark And Full Of Terrors.']});
+    expect(getCharacter('Neita')).toBeUndefined();
+    // expect(getCharacter('Brienne')).toBe({name: 'Arya Stark', class: 'Rogue', phrases: [ 'Not today', 'A girl has no name.' ]});
     // ESCREVA SEUS TESTES ABAIXO:
     // 1. Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     // 2. Teste se a função retorna o objeto correto para o parâmetro 'Arya',
